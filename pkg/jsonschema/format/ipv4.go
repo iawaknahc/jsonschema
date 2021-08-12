b2c1ffd9a5816e4fd534ba8d72f8334aab69b1c9
Louis Chan <louischan0325@gmail.com>
2021-08-12 23:20:50 +0800

    Validate IPv4 more strictly

--- pkg/jsonschema/format/ipv4.go
+++ pkg/jsonschema/format/ipv4.go
@@ -2,7 +2,9 @@ package format
 
 import (
 	"errors"
-	"net"
+	"fmt"
+	"strconv"
+	"strings"
 )
 
 var ErrInvalidIPV4Address = errors.New("invalid IPv4 address")
@@ -16,13 +18,29 @@ func (_ IPV4) CheckFormat(value interface{}) error {
 	if !ok {
 		return nil
 	}
-	ip := net.ParseIP(str)
-	if ip == nil {
+
+	// net.ParseIP accept 0127.0.0.1 as valid IP address.
+	// So here we parse the IP address strictly.
+	parts := strings.Split(str, ".")
+	if len(parts) != 4 {
 		return ErrInvalidIPV4Address
 	}
-	ipv4 := ip.To4()
-	if ipv4 == nil {
-		return ErrInvalidIPV4Address
+
+	for _, part := range parts {
+		// We first let strconv.ParseInt to accept number in any bases.
+		v, err := strconv.ParseInt(part, 0, 64)
+		if err != nil {
+			return ErrInvalidIPV4Address
+		}
+		if v < 0 || v > 255 {
+			return ErrInvalidIPV4Address
+		}
+		// And then verify part is in base 10.
+		decimal := fmt.Sprintf("%d", v)
+		if decimal != part {
+			return ErrInvalidIPV4Address
+		}
 	}
+
 	return nil
 }
